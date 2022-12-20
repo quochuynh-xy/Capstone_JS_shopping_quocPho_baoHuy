1c344451fbbc67423d2b7793ff2b88c25e115860
Quoc Pho <huynhquocpho@gmail.com>
2022-12-20 16:10:48 +0700

    sửa lỗi cú pháp

--- js/src/products.js
+++ js/src/products.js
@@ -9,13 +9,13 @@ function Products(
     _type,
     _stock
   ) {
-    (this.name = _name),
-      (this.price = _price),
-      (this.screen = _screen),
-      (this.backCamera = _backCamera),
-      (this.frontCamera = _frontCamera),
-      (this.img = _img),
-      (this.desc = _desc),
-      (this.type = _type),
-      (this.stock = _stock)
+    this.name = _name;
+      this.price = _price;
+      this.screen = _screen;
+      this.backCamera = _backCamera;
+      this.frontCamera = _frontCamera;
+      this.img = _img;
+      this.desc = _desc;
+      this.type = _type;
+      this.stock = _stock;
   }
